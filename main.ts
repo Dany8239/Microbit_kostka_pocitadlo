a42f21e8556eb3853aa61712de8bed80d794bb1f
Dany8239 <73122549+Dany8239@users.noreply.github.com>
2023-01-20 10:38:21 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    if (Mode == 0) {
+        Score += -1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Mode == 0) {
+        Mode = 1
+    } else {
+        Mode = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (Mode == 0) {
+        Score += 1
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (Mode == 1) {
+        RandomNum = randint(1, 6)
+    }
+})
+let RandomNum = 0
+let Mode = 0
+Mode = 0
+let Score = 0
+basic.forever(function () {
+    if (Mode == 0) {
+        basic.showNumber(Score)
+    } else {
+        basic.showNumber(RandomNum)
+    }
+})
